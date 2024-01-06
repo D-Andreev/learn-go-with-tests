84abb71bdb865ad502c07f0f4a5a39736c6b12b7
D-Andreev <d.andreev90@gmail.com>
2024-01-06 10:42:46 +0200

    Hello

---
+++ hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
